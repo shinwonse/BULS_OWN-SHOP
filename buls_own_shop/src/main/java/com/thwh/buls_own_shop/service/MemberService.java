1e5dd22dff0908baff3270ee9b8d2754a613c747
Jeong-EuiJae <47394504+cnpcnp99@users.noreply.github.com>
2021-01-20 01:02:23 +0900

    210120/Member관련/update

--- buls_own_shop/src/main/java/com/thwh/buls_own_shop/service/MemberService.java
+++ buls_own_shop/src/main/java/com/thwh/buls_own_shop/service/MemberService.java
@@ -1,5 +1,7 @@
 package com.thwh.buls_own_shop.service;
 
+import com.thwh.buls_own_shop.controller.MemberForm;
+import com.thwh.buls_own_shop.domain.Address;
 import com.thwh.buls_own_shop.domain.Member;
 import com.thwh.buls_own_shop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,16 +21,25 @@ public class MemberService {
      * 회원 가입
      */
     @Transactional
-    public Long signUp(Member member) {
-        validateDuplicateMember(member); //
-        memberRepository.save(member);
+    public Long signUp(MemberForm memberForm) {
+        Address address = new Address(memberForm.getCity(), memberForm.getStreet());
+        Member member = new Member();
+
+        member.setUser_id(memberForm.getUser_id());
+        member.setName(memberForm.getName());
+        member.setBirth(memberForm.getBirth());
+        member.setPhoneNumber(memberForm.getPhoneNumber());
+        member.setAddress(address);
+
+        validateDuplicateMember(member); // 아이디 중복되는 회원 체크
+        memberRepository.save(member);   // 레포지토리에 저장(DB로 슝)
         return member.getOriginal_id();
     }
 
     private void validateDuplicateMember(Member member) {
-        List<Member> members = memberRepository.findById(member.getName());
+        List<Member> members = memberRepository.findById(member.getUser_id());
         if (!members.isEmpty()) {
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
+            throw new IllegalStateException("이미 존재하는 ID입니다.");
         }
     }
 
