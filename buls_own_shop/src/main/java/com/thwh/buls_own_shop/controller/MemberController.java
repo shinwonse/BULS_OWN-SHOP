ab454677bce32bedfdf2f9bf5e1d95328c927e04
Jeong-EuiJae <47394504+cnpcnp99@users.noreply.github.com>
2021-01-19 22:45:23 +0900

    210119/html파일들/up+cr

--- buls_own_shop/src/main/java/com/thwh/buls_own_shop/controller/MemberController.java
+++ buls_own_shop/src/main/java/com/thwh/buls_own_shop/controller/MemberController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,6 +15,7 @@ public class MemberController {
         model.addAttribute("memberForm", new MemberForm());
         return "members/newMember";
     }
+
 
     @GetMapping("/members/login")
     public String loginForm(Model model) {
