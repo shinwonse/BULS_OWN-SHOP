764f20b2cd6748971a2f4fd57c85d80b046f3895
Jeong-EuiJae <47394504+cnpcnp99@users.noreply.github.com>
2021-01-20 00:36:23 +0900

    210120/Member관련/up+cr

---
+++ buls_own_shop/src/test/java/com/thwh/buls_own_shop/service/MemberServiceTest.java
@@ -0,0 +1,21 @@
+package com.thwh.buls_own_shop.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    @Test
+    void signUp() {
+        
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
