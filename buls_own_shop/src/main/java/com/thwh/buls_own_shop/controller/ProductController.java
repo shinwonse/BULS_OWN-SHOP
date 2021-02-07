2c5efb01eac76908110f497ccac00866f5e552d6
wonse <sinwonse@gmail.com>
2021-02-07 22:00:32 +0900

    test

--- buls_own_shop/src/main/java/com/thwh/buls_own_shop/controller/ProductController.java
+++ buls_own_shop/src/main/java/com/thwh/buls_own_shop/controller/ProductController.java
@@ -1,10 +1,15 @@
 package com.thwh.buls_own_shop.controller;
 
+import com.thwh.buls_own_shop.domain.product.Glove;
+import com.thwh.buls_own_shop.domain.product.Product;
 import com.thwh.buls_own_shop.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,16 +19,30 @@ public class ProductController {
 
     @GetMapping("/product/glove")
     public String glove_list(Model model) {
-        return "product/glove/glove_main";
+        Glove glove = new Glove();
+        glove.setName("제트");
+
+        List<Product> gloves = productService.findProducts();
+        model.addAttribute("items", gloves);
+        return "/product/glove/glove_list";
     }
 
     @GetMapping("/product/bat")
     public String bat_list(Model model) {
-        return "product/bat/bat_main";
+        List<Product> bats = productService.findProducts();
+        model.addAttribute("items", bats);
+        return "/product/bat/bat_list";
     }
 
     @GetMapping("/product/spike")
     public String spike_list(Model model) {
-        return "product/spike/spike_main";
+        List<Product> spikes = productService.findProducts();
+        model.addAttribute("items", spikes);
+        return "/product/spike/spike_list";
     }
+
+//    @GetMapping("/product/glove/{glovdId}/buy")
+//    public String glove_buy(@PathVariable("gloveId") Long gloveId, Model model) {
+//
+//    }
 }
